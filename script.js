5f8321210783a8f1ebd3d0a7bab3fa8c1ce0f95b
CardosoMarco <cardosomarco@example.com>
2025-08-08 05:59:04 -0400

    Add newsletter 08-08-2025

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Lista de newsletters disponíveis
 const newsletters = [
     {
+        date: "08-08-2025",
+        description: "OpenAI lança GPT-5 e muda o jogo, Zamp fechar capital, SoftBank US$ 2,87 bi IA"
+    },
+    {
         date: "07-08-2025",
         description: "Brasileiros ricos sem saída, Inter 40 mi clientes, América Latina década perdida"
     },
